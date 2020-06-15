ab7e04508a16f1ed3df43789ddf630ddd3244853
bunny944 <38912033+bunny944@users.noreply.github.com>
2020-06-15 16:41:09 +0530

    commit class_based_views

---
+++ api_basic/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ArticleAPIView, ArticleDetails
+
+
+urlpatterns = [
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/',ArticleDetails.as_view())
+]
+
